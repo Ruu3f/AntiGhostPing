d24a9ef80a2c970c9527f8bd126107e5fc1511b2
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-03-05 00:34:55 +0530

    explained the code

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
-import discord
-from discord.ext import commands
-import os
+import discord #importing py-cord
+from discord.ext import commands #importing commands
 
-intents = discord.Intents.all()
-client = commands.Bot(command_prefix='!', intents=intents)
+intents = discord.Intents.all() #setting intents, in this case i've set it to discord.Intents.all() so all intents are enabled
+client = commands.Bot(command_prefix='!', intents=intents) #setting the client value, this especially prefix and intents
 
-@client.event
-async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+@client.event #making a new event decorator
+async def on_ready(): #checking when the bot is started
+    print('Logged in as {0.user}'.format(client)) #telling what is the bot's user information
 
-client.load_extension('verification')
+client.load_extension('verification') #loading the verification cog
 
-client.run("${{ secrets.DISCORD_TOKEN }}")
+client.run("${{ secrets.DISCORD_TOKEN }}") #running the bot
