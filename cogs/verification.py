f3534dd4c65f10b517c2334c38824667f5c70d73
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-03-05 16:56:17 +0530

    Update verification.py

--- cogs/verification.py
+++ cogs/verification.py
@@ -21,11 +21,6 @@ class Verification(commands.Cog):
             await ctx.send(embed=embed)
             return
 
-        if self.verification_role in ctx.author.roles:
-            embed = discord.Embed(description="You have already been verified.")
-            await ctx.send(embed=embed)
-            return
-
         embed = discord.Embed(title="Verification", description="Click the button below to verify!", color=discord.Color.green())
         button = discord.ui.Button(label="Verify", style=discord.ButtonStyle.green)
         view = discord.ui.View(timeout=None)
@@ -33,6 +28,10 @@ class Verification(commands.Cog):
 
         async def button_callback(interaction: discord.Interaction):
             if interaction.user != ctx.author:
+                return
+            if self.verification_role in ctx.author.roles:
+                embed = discord.Embed(description="You have already been verified.")
+                await ctx.send(embed=embed)
                 return
 
             await interaction.response.send_message("You have been verified!", ephemeral=True)
