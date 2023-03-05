d551db2d9286d92c11187cfcc012c25b1bd012fa
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-03-05 16:41:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,8 +26,8 @@ async def on_ready():
     # call the client.change_presence() method to update the bot's presence in Discord. This sets the bot's status to online and its activity to the activity object created in the previous line.
     await client.change_presence(status=discord.Status.online, activity=activity)
 
-# load a custom extension called 'verification'
-client.load_extension('verification')
+# load a custom extension (cog) called 'verification'
+client.load_extension('cogs.verification')
 
 # call the client.run() method to start the bot and connect it to Discord using the TOKEN variable as the authentication key
 client.run(TOKEN)
