d73087ad87c9eb42a0ce6385068c5faba5f97865
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-03-05 16:40:53 +0530

    Update verification.py

--- verification.py
+++ verification.py
@@ -1,22 +1,25 @@
 import discord
 from discord.ext import commands
 
-
 class Verification(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.verification_role = None
+        self.verification_role: discord.Role = None
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def set_verification_role(self, ctx, role: discord.Role):
+    async def set_verification_role(self, ctx: commands.Context, role: discord.Role):
         self.verification_role = role
         await ctx.send(f"Verification role set to {role.name}.")
 
     @commands.command()
-    async def send_verification(self, ctx):
+    async def send_verification(self, ctx: commands.Context):
         if self.verification_role is None:
             await ctx.send("Verification role not set.")
+            return
+
+        if self.verification_role in ctx.author.roles:
+            await ctx.send("You have already been verified.")
             return
 
         embed = discord.Embed(title="Verification", description="Click the button below to verify!", color=discord.Color.green())
@@ -34,11 +37,13 @@ class Verification(commands.Cog):
 
             await interaction.message.delete()
 
+            dm_channel = await interaction.user.create_dm()
+            await dm_channel.send("You have been verified!")
+
         button.callback = button_callback
 
         await ctx.message.delete()
         await ctx.send(embed=embed, view=view, delete_after=10)
 
-
-def setup(bot):
+def setup(bot: commands.Bot) -> None:
     bot.add_cog(Verification(bot))
